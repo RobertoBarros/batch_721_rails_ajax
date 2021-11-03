a5b279c0ccc2fbb7c4132711843a84758f29b047
Roberto Barros <roberto.f.barros@gmail.com>
2021-11-03 11:07:50 -0300

    stimulus + fetch

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "controllers"
 
 Rails.start()
 Turbolinks.start()
@@ -23,6 +24,8 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+
+
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :restaurants, only: [:index, :show] do
+  resources :restaurants, only: [:index, :show, :destroy] do
     resources :reviews, only: [:create]
   end
+
+  # resources :reviews, only: [:destroy]
 
   root to: 'restaurants#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
